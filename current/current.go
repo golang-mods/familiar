7fbcac4a633edc762def0de3451072c72e31f012
mondy <mondy.dev@gmail.com>
2022-03-23 17:41:26 +0900

    add Function function

--- current/current.go
+++ current/current.go
@@ -14,3 +14,9 @@ func Directory() string {
 	_, file, _, _ := runtime.Caller(1)
 	return filepath.Dir(file)
 }
+
+func Function() string {
+	pc, _, _, _ := runtime.Caller(1)
+	function := runtime.FuncForPC(pc)
+	return function.Name()
+}
