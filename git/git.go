f5ae6b627f40fee2a4a88ad530a153d5953db965
mondy <mondy.dev@gmail.com>
2023-03-22 21:07:08 +0900

    Add empty validation to git commands

--- git/git.go
+++ git/git.go
@@ -1,19 +1,41 @@
 package git
 
-import "github.com/golang-mods/familiar/shell"
+import (
+	"errors"
+
+	"github.com/golang-mods/familiar/shell"
+)
 
 func Revision(current string) (string, error) {
-	return shell.Output("git", "-C", current, "rev-parse", "HEAD")(shell.StderrSilent())
+	return validateEmpty(
+		shell.Output("git", "-C", current, "rev-parse", "HEAD")(shell.StderrSilent()),
+	)
 }
 
 func RevisionShort(current string) (string, error) {
-	return shell.Output("git", "-C", current, "rev-parse", "--short", "HEAD")(shell.StderrSilent())
+	return validateEmpty(
+		shell.Output("git", "-C", current, "rev-parse", "--short", "HEAD")(shell.StderrSilent()),
+	)
 }
 
 func Tag(current string) (string, error) {
-	return shell.Output("git", "-C", current, "tag", "--points-at", "HEAD")(shell.StderrSilent())
+	return validateEmpty(
+		shell.Output("git", "-C", current, "tag", "--points-at", "HEAD")(shell.StderrSilent()),
+	)
 }
 
 func Clean(current string) error {
 	return shell.Run("git", "-C", current, "clean", "-fdX")()
+}
+
+var ErrEmpty = errors.New("value is empty")
+
+func validateEmpty(value string, err error) (string, error) {
+	if err != nil {
+		return value, err
+	}
+	if value == "" {
+		return "", ErrEmpty
+	}
+	return value, err
 }
